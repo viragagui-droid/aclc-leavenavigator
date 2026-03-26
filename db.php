e5c1b3361456098f6a8cf403475a0e9874546a1e
viragagui-droid <viragagui@gmail.com>
2026-03-26 19:10:42 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 $host = "mysql-314fa574-viragagui-af01.a.aivencloud.com";
 $user = "avnadmin";
-$pass = "AVNS_W5Nle2VJ42Qy1-Hrc5V"; // I-click ang 'Reveal Password' sa Aiven
+$pass = "AVNS_WSNle2VJ42Qy1-Hrc5V"; 
 $db   = "defaultdb";
 $port = "14398";
 
-// Importante sa Aiven: Kailangan ng SSL connection
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
+// Ito ang magko-connect sa Vercel papunta sa Aiven
 if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
 }
