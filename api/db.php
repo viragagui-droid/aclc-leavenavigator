ab335a3880cc5630b02516343f54d8a80d8ab627
viragagui-droid <viragagui@gmail.com>
2026-03-26 19:20:28 +0800

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,7 +1,15 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "leave_navigator_db"); // Palitan ang db name kung iba ang gamit mo
+$host = "mysql-314fa574-viragagui-af01.a.aivencloud.com";
+$user = "avnadmin";
+$pass = "AVNS_W5Nle2VJ42Qy1-Hrc5V"; // I-reveal mo ang password sa Aiven dashboard
+$db   = "defaultdb";
+$port = "14398";
 
-if (!$conn) {
+$conn = mysqli_init();
+// Kailangan ang SSL para sa Aiven
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
